e981d328ed8a962976ff6173aa03dc7049a3e1c1
andallves <andrealves10a@gmail.com>
2023-06-14 14:13:11 -0300

    removing the challenges

--- pythonAula/Aula009.py
+++ pythonAula/Aula009.py
@@ -98,34 +98,3 @@ print(phrase.upper().count('A'))
 
 # usando o método len para saber quantos caracteres há no phrase
 print(len(phrase))
-
-"""
-============ DESAFIOS ============
-
-
-# Faça um program que leia um número de 0 a 9999 e mostre na tela cada um dos
-digitos separados.
-    Ex:
-    - Digite um número: 1834
-    - unidade: 4
-    - dezena: 3
-    - centena: 8
-    - milhar: 1
-
-# Crie um programa que leia o nome de uma cidade e diga se ela começa
-ou não com o nome "SANTO".
-
-# Crie um programa que leia o nome de uma pessoa e diga se ela
-tem "SILVA" no nome.
-
-# Faça um programa que leia uma frase pelo teclado e mostre:
-    - Quantas vezes aparece a letra "A";
-    - Em que posição ela aparece a primeira vez;
-    - Em que posição ela aparece a última vez.
-
-# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida
-o primeiro e o último nome separadamente.
-    Ex: Ana Maria de Sousa
-    - primeiro = Ana
-    - último = Souza
-"""
