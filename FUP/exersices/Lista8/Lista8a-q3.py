c96644b6ceaf625fc180d38f77e2c1f33cb7324d
andallves <andrealves10a@gmail.com>
2023-06-20 17:46:15 -0300

    question 3 by list 8a

---
+++ FUP/exersices/Lista8/Lista8a-q3.py
@@ -0,0 +1,18 @@
+"""
+Faça uma função em python para gerar um vetor de 10 valores inteiros e informar
+o maior valor deste, recursivamente.
+"""
+from random import randint
+
+
+def generateVector(vect):
+    if len(vect) == 10:
+        return max(vect)
+
+    (vect.append(randint(1, 99)))
+
+    return generateVector(vect)
+
+
+vect = []
+print(generateVector(vect))
